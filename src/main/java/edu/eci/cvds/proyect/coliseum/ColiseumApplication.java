7445e837640a1e308b17b85a55750d319575313a
David Castro <david.csierra@mail.escuelaing.edu.co>
2025-04-29 18:21:16 -0500

    estructura

---
+++ src/main/java/edu/eci/cvds/proyect/coliseum/ColiseumApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.proyect.coliseum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColiseumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColiseumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/cvds/proyect/coliseum/ColiseumApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.proyect.coliseum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColiseumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
